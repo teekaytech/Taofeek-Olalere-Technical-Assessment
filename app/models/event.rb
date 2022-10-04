827bd9c634bc298e2a3a488087384dbbde760f36
Taofeek Olalere <olaleretaofeek@live.com>
2022-10-04 21:07:03 +0100

    Add necessary validation for event start and end dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   validates :status, :category, :start_date, :end_date, presence: true
+  validate :start_and_end_dates
 
   enum :status, [ :active, :inactive ], prefix: true, scopes: false, default: :active
   enum :category, [ :free, :paid ], prefix: true, scopes: false, default: :free
@@ -16,4 +17,15 @@ class Event < ApplicationRecord
   scope :with_category, -> (category) { category == 'free' ? free : paid if category.present? }
   scope :free, -> { where(category: :free) }
   scope :paid, -> { where(category: :paid) }
+
+  def start_and_end_dates
+    if start_date > end_date
+      errors.add(:start_date, 'must be before end date')
+      errors.add(:end_date, 'must be after start date')
+    elsif start_date < Date.today
+      errors.add(:start_date, 'must be in the future')
+    elsif end_date < Date.today
+      errors.add(:end_date, 'must be in the future')
+    end
+  end
 end
